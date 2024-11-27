5c7e824a9098f929d608d112d3a79c29250e2f9d
imran.sekh <imransekh25@gmail.com>
2024-11-27 17:49:01 +0530

    Reactive Project

---
+++ src/main/java/com/botbel/avenger/AvengerApplication.java
@@ -0,0 +1,13 @@
+package com.botbel.avenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botbel/avenger/AvengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botbel.avenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
